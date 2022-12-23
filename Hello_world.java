b6835a268980e185bfe8f9eb015b3818e596f640
vijay sonulkar <you@example.comvijay.sonulkar@gmail.com>
2022-12-23 19:22:15 +0530

    hellow world program added

---
+++ Hello_world.java
@@ -0,0 +1,9 @@
+package basics;
+
+public class Hello_world {
+	public static void main(String[] args) {
+		System.out.println("hello world");
+		System.out.println("welcome to java programming");
+	}
+
+}
